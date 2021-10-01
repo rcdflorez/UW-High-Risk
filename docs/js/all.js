aa2ac2668e65cd73b53b2dc44a815d27928a4fda
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2021-10-01 15:47:42 -0400

    Lend Mate Fix

--- docs/js/all.js
+++ docs/js/all.js
@@ -44,7 +44,7 @@ var slideCount = $("#slider ul li").length;
 var slideWidth = $("#step-nav").width() + 32; //$("#slider ul li").width();
 var slideHeight = $("div.step-3").innerHeight();
 var sliderUlWidth = slideCount * slideWidth;
-var ulInitialHeight = 540; //$("#slider ul li:first-child").height();
+var ulInitialHeight = 600; //$("#slider ul li:first-child").height();
 
 var navContainerWidth =
   $("#nav-steps-container").outerWidth() -
@@ -158,7 +158,9 @@ $("a.uploadLink").click(function (e) {
 
 var handler = BerbixVerify.configure({
   onComplete: function () {
-    alert("Verification Complete.");
+    $("#berbixArea").html(
+      '<h1 class="heading-title mb-5">  Thanks for completing the ID check</h1><p class="mb-5">Now proceed with bank verification.</p><svg class="checkmark mx-auto mb-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><a onClick="(function(){moveRight()})();return false;" href="" class="btn justify-content-center btn btn-access mb-3 verification-completed"> Continue</a>'
+    );
     startLendMateFlow();
   },
   onExit: function () {
@@ -170,24 +172,34 @@ var hosted = false;
 var testName = "Ricardo's test";
 var loanId = parseInt(Math.random() * 100000);
 var verificationDomain = "https://d37e-99-158-137-142.ngrok.io"; //"https://vportaltest.explorecredit.com";
-
+/*
 $.ajax(`${verificationDomain}/api/uw_flow/berbix/createClientToken`, {
   method: "POST",
   crossDomain: true,
+  beforeSend: function () {
+    //
+  },
+  success: function () {},
   data: {
     hosted,
     test_name: testName,
     loan_id: loanId,
   },
-}).done((data) => {
-  clientToken = data.client_token;
+})
+  .done((data) => {
+    clientToken = data.client_token;
 
-  handler.open({
-    clientToken: clientToken,
-    modal: false,
-    root: "berbixArea",
-  });
-});
+    handler.open({
+      clientToken: clientToken,
+      modal: false,
+      root: "berbixArea",
+    });
+  })
+  .then(() => {
+    $("div.BerBix-place-holder").fadeOut("slow", function () {
+      $(this).html("");
+    });
+  });*/
 
 function startLendMateFlow() {
   $.ajax(`${verificationDomain}/api/uw_flow/lendmate/createClientToken`, {
