72a6a535104b6bcfbc89d5443283abf95009703e
Ricardo Florez <system@leapcredit.com>
2021-10-04 15:24:14 -0400

    Update services end-point

--- src/js/index.js
+++ src/js/index.js
@@ -38,21 +38,21 @@ var sliderUlWidth = slideCount * slideWidth;
 var ulInitialHeight = 615; // $("#slider ul li:first-child").height();
 
 var navContainerWidth =
-  $("#nav-steps-container").outerWidth() -
-  parseInt($("#slider").css("padding-left")) / 2;
+    $("#nav-steps-container").outerWidth() -
+    parseInt($("#slider").css("padding-left")) / 2;
 
 console.log(navContainerWidth);
 
 var marginLeftCalculation = 0;
 var desktopMarginCalulation =
-  $("#testDiv").innerWidth() +
-  parseInt($("#testDiv").css("padding-left")) * 3 +
-  4;
+    $("#testDiv").innerWidth() +
+    parseInt($("#testDiv").css("padding-left")) * 3 +
+    4;
 
 if (window.screen.width > 800) {
-  marginLeftCalculation = desktopMarginCalulation;
+    marginLeftCalculation = desktopMarginCalulation;
 } else {
-  marginLeftCalculation = navContainerWidth;
+    marginLeftCalculation = navContainerWidth;
 }
 
 var currentStep = 0;
@@ -66,156 +66,154 @@ $("div.slider").css({ width: navWidth });
 $("#slider").css({ width: navWidth });
 
 $("#slider ul").css({
-  width: sliderUlWidth,
-  marginLeft: -marginLeftCalculation,
-  height: ulInitialHeight,
+    width: sliderUlWidth,
+    marginLeft: -marginLeftCalculation,
+    height: ulInitialHeight,
 });
 
 $("#slider ul li:last-child").prependTo("#slider ul");
 
 function moveLeft() {
-  $("#slider ul").animate(
-    {
-      left: +slideWidth,
-    },
-    200,
-    function () {
-      $("#slider ul li:last-child").prependTo("#slider ul");
-      $("#slider ul").css("left", "");
-    }
-  );
+    $("#slider ul").animate({
+            left: +slideWidth,
+        },
+        200,
+        function() {
+            $("#slider ul li:last-child").prependTo("#slider ul");
+            $("#slider ul").css("left", "");
+        }
+    );
 }
 
 function moveRight() {
-  //$(".step-nav .container a")[currentStep].addClass("step-ok");
+    //$(".step-nav .container a")[currentStep].addClass("step-ok");
 
-  if (currentStep === 2) return;
+    if (currentStep === 2) return;
 
-  var completedStepNav = $(".step-nav .container a")[currentStep];
+    var completedStepNav = $(".step-nav .container a")[currentStep];
 
-  $(completedStepNav).addClass("step-ok");
+    $(completedStepNav).addClass("step-ok");
 
-  var nextHeightValue = $("#slider ul li:last-child").height();
-  console.log(nextHeightValue);
-  $("#slider ul").css({ height: nextHeightValue });
-  $("#slider ul").animate(
-    {
-      left: -slideWidth,
-    },
-    200,
-    function () {
-      $("#slider ul li:first-child").appendTo("#slider ul");
-      $("#slider ul").css("left", "");
-    }
-  );
-  currentStep++;
+    var nextHeightValue = $("#slider ul li:last-child").height();
+    console.log(nextHeightValue);
+    $("#slider ul").css({ height: nextHeightValue });
+    $("#slider ul").animate({
+            left: -slideWidth,
+        },
+        200,
+        function() {
+            $("#slider ul li:first-child").appendTo("#slider ul");
+            $("#slider ul").css("left", "");
+        }
+    );
+    currentStep++;
 
-  var currentStepNav = $(".step-nav .container a")[currentStep];
-  $(currentStepNav).addClass("active-nav");
+    var currentStepNav = $(".step-nav .container a")[currentStep];
+    $(currentStepNav).addClass("active-nav");
 }
 
 try {
-  document.getElementById("phone").addEventListener("input", function (e) {
-    var x = e.target.value
-      .replace(/\D/g, "")
-      .match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-    e.target.value = !x[2]
-      ? x[1]
-      : "(" + x[1] + ") " + x[2] + (x[3] ? "-" + x[3] : "");
-  });
+    document.getElementById("phone").addEventListener("input", function(e) {
+        var x = e.target.value
+            .replace(/\D/g, "")
+            .match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        e.target.value = !x[2] ?
+            x[1] :
+            "(" + x[1] + ") " + x[2] + (x[3] ? "-" + x[3] : "");
+    });
 } catch (e) {}
 
-$("div.step-1 a.verification-completed").click(function (e) {
-  e.preventDefault();
-  moveRight();
+$("div.step-1 a.verification-completed").click(function(e) {
+    e.preventDefault();
+    moveRight();
 });
-$("div.step-1 a.start-verification").click(function (e) {
-  e.preventDefault();
-  $(this).unbind("click", arguments.callee);
-  BerbixHandOffToPhone();
-  setTimeout(function () {
-    BerbixVerificationCompleted();
-  }, 3500);
+$("div.step-1 a.start-verification").click(function(e) {
+    e.preventDefault();
+    $(this).unbind("click", arguments.callee);
+    BerbixHandOffToPhone();
+    setTimeout(function() {
+        BerbixVerificationCompleted();
+    }, 3500);
 });
 
-$("a.uploadLink").click(function (e) {
-  console.log("Holi");
-  e.preventDefault();
-  $("div.step-2")
-    .load("upload-test.html", function () {})
-    .hide()
-    .fadeIn();
+$("a.uploadLink").click(function(e) {
+    console.log("Holi");
+    e.preventDefault();
+    $("div.step-2")
+        .load("upload-test.html", function() {})
+        .hide()
+        .fadeIn();
 });
 
 var handler = BerbixVerify.configure({
-  onComplete: function () {
-    $("#berbixArea").html(
-      '<h1 class="heading-title mb-5">  Thanks for completing the ID check</h1><p class="mb-5">Now proceed with bank verification.</p><svg class="checkmark mx-auto mb-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><a onClick="(function(){moveRight()})();return false;" href="" class="btn justify-content-center btn btn-access mb-3 verification-completed"> Continue</a>'
-    );
-    startLendMateFlow();
-  },
-  onExit: function () {
-    alert("Verification exited.");
-  },
+    onComplete: function() {
+        $("#berbixArea").html(
+            '<h1 class="heading-title mb-5">  Thanks for completing the ID check</h1><p class="mb-5">Now proceed with bank verification.</p><svg class="checkmark mx-auto mb-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><a onClick="(function(){moveRight()})();return false;" href="" class="btn justify-content-center btn btn-access mb-3 verification-completed"> Continue</a>'
+        );
+        startLendMateFlow();
+    },
+    onExit: function() {
+        alert("Verification exited.");
+    },
 });
 
 var hosted = false;
 var testName = "Ricardo's test";
 var loanId = parseInt(Math.random() * 100000);
-var verificationDomain = "https://d37e-99-158-137-142.ngrok.io"; //"https://vportaltest.explorecredit.com";
+var verificationDomain = "https://bcb9-99-158-137-142.ngrok.io"; //"https://vportaltest.explorecredit.com";
 
 $.ajax(`${verificationDomain}/api/uw_flow/berbix/createClientToken`, {
-  method: "POST",
-  crossDomain: true,
-  beforeSend: function () {
-    //
-  },
-  success: function () {},
-  data: {
-    hosted,
-    test_name: testName,
-    loan_id: loanId,
-  },
-})
-  .done((data) => {
-    clientToken = data.client_token;
+        method: "POST",
+        crossDomain: true,
+        beforeSend: function() {
+            //
+        },
+        success: function() {},
+        data: {
+            hosted,
+            test_name: testName,
+            loan_id: loanId,
+        },
+    })
+    .done((data) => {
+        clientToken = data.client_token;
 
-    handler.open({
-      clientToken: clientToken,
-      modal: false,
-      root: "berbixArea",
+        handler.open({
+            clientToken: clientToken,
+            modal: false,
+            root: "berbixArea",
+        });
+    })
+    .then(() => {
+        $("div.BerBix-place-holder").fadeOut("slow", function() {
+            $(this).html("");
+        });
     });
-  })
-  .then(() => {
-    $("div.BerBix-place-holder").fadeOut("slow", function () {
-      $(this).html("");
-    });
-  });
 
 function startLendMateFlow() {
-  $.ajax(`${verificationDomain}/api/uw_flow/lendmate/createClientToken`, {
-    method: "POST",
-    crossDomain: true,
-    data: {
-      loan_id: loanId,
-    },
-  }).done((data) => {
-    data.widget_link;
+    $.ajax(`${verificationDomain}/api/uw_flow/lendmate/createClientToken`, {
+        method: "POST",
+        crossDomain: true,
+        data: {
+            loan_id: loanId,
+        },
+    }).done((data) => {
+        data.widget_link;
 
-    let lmFrame = $("#lendMateContainer");
-    let iframeSource = data.widget_link;
-    lmFrame.attr("src", iframeSource);
+        let lmFrame = $("#lendMateContainer");
+        let iframeSource = data.widget_link;
+        lmFrame.attr("src", iframeSource);
 
-    window.onmessage = (event) => {
-      console.log(`Received message: ${event.data}`);
-      console.log(event);
-      if (event.data.success) {
-        setTimeout(function () {
-          $("div.step-2").html(
-            '<svg class="checkmark mx-auto mb-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><h1 class="my-sm-4 heading-title col-md-11 mx-auto mb-5">Your Account is successfully verified and linked!</h1><a onClick="(function(){moveRight()})();return false;" href="" class="btn justify-content-center btn btn-access mb-3 verification-completed"> Continue</a>'
-          );
-        }, 2000);
-      }
-    };
-  });
+        window.onmessage = (event) => {
+            console.log(`Received message: ${event.data}`);
+            console.log(event);
+            if (event.data.success) {
+                setTimeout(function() {
+                    $("div.step-2").html(
+                        '<svg class="checkmark mx-auto mb-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><h1 class="my-sm-4 heading-title col-md-11 mx-auto mb-5">Your Account is successfully verified and linked!</h1><a onClick="(function(){moveRight()})();return false;" href="" class="btn justify-content-center btn btn-access mb-3 verification-completed"> Continue</a>'
+                    );
+                }, 2000);
+            }
+        };
+    });
 }
